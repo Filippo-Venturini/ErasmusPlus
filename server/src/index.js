868d9de9a4d4a4950f035440bb2a10bb5814635e
Alberto Di Girolamo <alberto.digirolamo2@studio.unibo.it>
2023-05-20 11:33:04 +0200

    add db connect

--- server/src/index.js
+++ server/src/index.js
@@ -2,3 +2,14 @@ const express = require('express');
 const app = express();
 
 app.use(express.json());
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/erasmusPlusDB').then(() => {
+    console.log('Connected to DB');
+}).catch(err => {
+    console.log(err);
+});
+
+app.listen(3000, () => {
+    console.log('listening on http://localhost:3000');
+});
