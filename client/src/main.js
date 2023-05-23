bea08d7cfbf8f7a29d6b2cd8a03f352674574e19
Alberto Di Girolamo <alberto.digirolamo2@studio.unibo.it>
2023-05-23 12:47:03 +0200

    add client

---
+++ client/src/main.js
@@ -0,0 +1,11 @@
+import App from "./App.vue";
+import {createApp} from 'vue';
+import router from "./router/router";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap"
+
+const app = createApp(App);
+
+app.use(router);
+app.mount('#app');
+
---
+++ client/src/pages/HomePage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>ciao</div>
+  <Button></Button>
+</template>
+
+<script>
+import Button from "../components/Button.vue";
+
+export default {
+  name: "HomePage",
+  components: {Button}
+}
+</script>
+
+<style scoped>
+
+</style>
