25dac6dba894a1872514a02aa5cfa68ef0e64ea1
Filippo Venturini <filippo.venturini8@studio.unibo.it>
2023-05-23 13:19:26 +0200

    Setup the header

---
+++ client/src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <header class="header">
+    <img class="logo" src="src/assets/img/logoErasmusPlus.png">
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header"
+}
+</script>
+
+<style scoped>
+.header{
+  background-color: #BB2E29;
+  height: 120px;
+}
+
+.logo{
+  height: 120px;
+}
+</style>
--- client/src/pages/HomePage.vue
+++ client/src/pages/HomePage.vue
@@ -1,14 +1,16 @@
 <template>
+  <Header></Header>
   <div>ciao</div>
   <Button></Button>
 </template>
 
 <script>
 import Button from "../components/Button.vue";
+import Header from "@/components/Header.vue";
 
 export default {
   name: "HomePage",
-  components: {Button}
+  components: {Header, Button}
 }
 </script>
 
