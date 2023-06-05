855bedb479b4cd74dad08c52a3c8a659b4128dfb
Pietro Lelli <pietro.lelli00@gmail.com>
2023-06-05 11:07:35 +0200

    modify users route on index

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const universitiesRouter = require('./routes/universitiesRoute');
+const usersRouter = require('./routes/usersRoute');
 
 const app = express();
 
 app.use(universitiesRouter);
+app.use(usersRouter);
 app.use(express.json());
 
 mongoose.connect('mongodb://127.0.0.1:27017/erasmusPlusDB').then(() => {
