5805c8f56f88148606819ff822f89426502faacd
Alberto Di Girolamo <alberto.digirolamo2@studio.unibo.it>
2023-06-08 15:29:51 +0200

    add request push

--- server/src/controllers/universitiesController.js
+++ server/src/controllers/universitiesController.js
@@ -2,6 +2,7 @@ const universitiesModel = require('../models/universitiesModel');
 const universityDetailModel = require("../models/universitiesModel");
 const addOfferModel = require("../models/universitiesModel");
 
+
 exports.all_universities = async(req, res) => {
     try{
         res.header("Access-Control-Allow-Origin", "*");
@@ -23,9 +24,20 @@ exports.university_detail = async(req, res) => {
 
 exports.add_offer = async (req, res)=> {
     res.header("Access-Control-Allow-Origin", "*");
-    const offer = new addOfferModel({     "name":"ciao",     "country":"ciao",     "city":"ciao",     "latitude":123,     "longitude":123,     "offer": {         "period":"ciao",         "places":"ciao",         "field":"ciao"     },     "logo":"ciao",     "wallpaper":"ciao",     "cardImg":"ciao",     "plot":"ciao" });
+    res.header('Access-Control-Allow-Methods', 'GET,PUT, POST,DELETE');
+    res.header("Access-Control-Allow-Headers", "Content-type,Accept,X-Custom-Header");
+
+    if (req.method === "OPTIONS") {
+        res.header('Access-Control-Allow-Origin', req.headers.origin);
+    } else {
+        res.header('Access-Control-Allow-Origin', '*');
+    }
+
+    const offer = new addOfferModel(req.body);
     console.log(req.body);
-    console.log("arrivata");
+    console.log(offer)
+
+
     try{
         res.json(await offer.save());
     }catch (e) {
