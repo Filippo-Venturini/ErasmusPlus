d1f2127ba2344a6f5d3369598ff0b7f2a32e443b
Filippo Venturini <filippo.venturini8@studio.unibo.it>
2023-06-12 11:28:16 +0200

    Add border to filters

--- client/src/components/home/FilterMenu.vue
+++ client/src/components/home/FilterMenu.vue
@@ -1,16 +1,16 @@
 <template>
 <div class="filter-menu row">
   <div class="col-1 d-flex justify-content-center align-items-center">
-    <i class="bi bi-bookmark-star filter-icon"></i>
+    <i class="bi bi-bookmark-star filter-icon" :class="{clicked: savedClicked}" @click="this.onSavedClicked()"></i>
   </div>
   <div class="col-1 d-flex justify-content-center align-items-center">
-    <i class="bi bi-book filter-icon"></i>
+    <i class="bi bi-book filter-icon" :class="{clicked: studyClicked}" @click="this.onStudyClicked()"></i>
   </div>
   <div class="col-1 d-flex justify-content-center align-items-center">
-    <i class="bi bi-circle-half filter-icon" @click="this.onHalfClicked()"></i>
+    <i class="bi bi-circle-half filter-icon" :class="{clicked: halfClicked}" @click="this.onHalfClicked()"></i>
   </div>
   <div class="col-1 d-flex justify-content-center align-items-center">
-    <i class="bi bi-circle-fill filter-icon" @click="this.onFullClicked()"></i>
+    <i class="bi bi-circle-fill filter-icon" :class="{clicked: fullClicked}" @click="this.onFullClicked()"></i>
   </div>
   <div class="col-6">
 
@@ -30,6 +30,14 @@ import {defineComponent} from "vue";
 export default defineComponent({
   name: "FilterMenu",
   emits: ["toMap", "toList"],
+  data(){
+    return{
+      savedClicked: false,
+      studyClicked: false,
+      halfClicked: false,
+      fullClicked: false
+    }
+  },
   methods:{
     switchToMap(){
       this.$emit('toMap', true);
@@ -37,10 +45,20 @@ export default defineComponent({
     switchToList(){
       this.$emit('toList', false);
     },
+    onSavedClicked(){
+      this.savedClicked = !this.savedClicked;
+    },
+    onStudyClicked(){
+      this.studyClicked = !this.studyClicked;
+    },
     onHalfClicked(){
+      this.halfClicked = !this.halfClicked;
+      this.fullClicked = false;
       this.$emit("filterClicked", "half");
     },
     onFullClicked(){
+      this.fullClicked = !this.fullClicked;
+      this.halfClicked = false;
       this.$emit("filterClicked", "full");
     }
   }
@@ -62,5 +80,12 @@ export default defineComponent({
 
 .filter-icon:hover{
   color: #000000;
+}
+
+.clicked{
+  color: #000000;
+  padding-left: 5px;
+  padding-right: 5px;
+  border: solid 2px;
 }
 </style>
