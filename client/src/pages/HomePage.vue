161c0fe2ffe7c30414d5e441e9adcb165f1168ef
Pietro Lelli <pietro.lelli00@gmail.com>
2023-06-13 11:33:03 +0200

    add button to switch view in homePage

--- client/src/pages/HomePage.vue
+++ client/src/pages/HomePage.vue
@@ -4,6 +4,9 @@
     <MapContainer v-if="toMap" @toList="switchView" :universities="universities" :key="universities.id"></MapContainer>
     <OffersContainer v-if="!toMap" @toMap="switchView" :universities="universities" :key="universities.id"></OffersContainer>
   </div>
+  <button v-if="toMap" @click="switchView(false)" style="background: #495057; color: #FFFFFF; height: 50px; width:180px; position: absolute; top: 99%; left: 47%; border-radius: 30px; font-size: 1.2rem;">Elenco <i class="bi bi-list" style="font-size: 1.5rem; margin-left: 20px"></i></button>
+  <button v-if="!toMap" @click="switchView(true)" style="background: #495057; color: #FFFFFF; height: 50px; width:180px; position: absolute; top: 99%; left: 47%; border-radius: 30px; font-size: 1.2rem">Mappa <i class="bi bi-map-fill" style="font-size: 1.5rem; margin-left: 20px"></i></button>
+
 </template>
 
 <script>
